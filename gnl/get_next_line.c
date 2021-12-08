8c51214edb141c87fadb9c77c4d9c89bfdd6c510
Emanuele Torrefiel <etorrefi@F3C021.42adel.org.au>
2021-12-09 09:16:13 +1030

    update

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -40,18 +40,22 @@ char *ft_substr(char *s, int start, int len)
 char *ft_strjoin(char *s1, char *s2)
 {
 	char *dest;
-	int i, j;
+	int i = 0, j = 0;
 	if (!s1 || !s2)
 		return NULL;
 	dest = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!dest)
 		return NULL;
-	i = -1;
-	while (s1[++i])
+	while (s1[i])
+	{
 		dest[i] = s1[i];
-	j = -1;
-	while (s2[++j])
+		i++;
+	}
+	while (s2[j])
+	{
 		dest[i + j] = s2[j];
+		j++;
+	}
 	free(s1);
 	dest[i + j] = '\0';
 	return dest;
